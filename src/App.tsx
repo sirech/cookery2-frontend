b7fabf38827fdfd7d590a81c9e8ac4141cdefbf7
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-08-20 22:36:52 +0200

    add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import CssBaseline from '@material-ui/core/CssBaseline'
+
 import NewRecipe from 'new-recipe'
 
 const App = () => (
   <main data-testid="app">
+    <CssBaseline />
+
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">Photos</Typography>
+      </Toolbar>
+    </AppBar>
+
     <Route path="/recipes/new" component={NewRecipe} />
   </main>
 )
