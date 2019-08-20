f248bf2f2668f6b103713583da2b99a243a71930
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-08-20 22:11:37 +0200

    one field form

--- src/new-recipe/NewRecipe.tsx
+++ src/new-recipe/NewRecipe.tsx
@@ -1,4 +1,30 @@
 import React from 'react'
 
-const NewRecipe = () => <section>stuff</section>
+import { Formik, Field, FormikProps } from 'formik'
+
+interface RecipeForm {
+  name: string
+}
+
+const initialValues: RecipeForm = {
+  name: ''
+}
+
+const NewRecipe = () => (
+  <section>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={values => console.log(values)}
+    >
+      {({ handleSubmit }: FormikProps<RecipeForm>) => (
+        <form onSubmit={handleSubmit}>
+          <h2>New Recipe</h2>
+          <Field name="name" />
+
+          <button type="submit">Create</button>
+        </form>
+      )}
+    </Formik>
+  </section>
+)
 export default NewRecipe
