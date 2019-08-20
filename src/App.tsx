696f157385a8c3106e19d1b6233caee9797b7777
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-08-20 22:02:07 +0200

    new recipe skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
-const App = () => <main>Stuff</main>
+import NewRecipe from 'new-recipe'
+
+const App = () => (
+  <main>
+    <Route path="/recipes/new" component={NewRecipe} />
+  </main>
+)
 export default App
---
+++ src/new-recipe/NewRecipe.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+
+const NewRecipe = () => <section>stuff</section>
+export default NewRecipe
