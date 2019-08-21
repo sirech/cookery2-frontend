539c19ffaff31d01f2bbe8ebcde866004400ae00
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-08-21 21:16:02 +0200

    basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,26 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import Container from '@material-ui/core/Container'
+import Box from '@material-ui/core/Box'
 
 import NewRecipe from 'new-recipe'
 
 const App = () => (
-  <main data-testid="app">
+  <div data-testid="app">
     <CssBaseline />
 
-    <AppBar position="static">
+    <AppBar>
       <Toolbar>
-        <Typography variant="h6">Photos</Typography>
+        <Typography variant="h6">Cookery</Typography>
       </Toolbar>
     </AppBar>
 
-    <Route path="/recipes/new" component={NewRecipe} />
-  </main>
+    <Box mt={12}>
+      <Container component="main">
+        <Route path="/recipes/new" component={NewRecipe} />
+      </Container>
+    </Box>
+  </div>
 )
 export default App
--- src/new-recipe/NewRecipe.tsx
+++ src/new-recipe/NewRecipe.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+
 import { Formik, Field, FormikProps } from 'formik'
+import { TextField } from 'formik-material-ui'
+
+import * as Yup from 'yup'
 
 interface RecipeForm {
   name: string
@@ -10,18 +16,33 @@ const initialValues: RecipeForm = {
   name: ''
 }
 
+const validationSchema = Yup.object().shape({ name: Yup.string().required() })
+
 const NewRecipe = () => (
   <section>
     <Formik
       initialValues={initialValues}
+      validationSchema={validationSchema}
       onSubmit={values => console.log(values)}
     >
       {({ handleSubmit }: FormikProps<RecipeForm>) => (
         <form onSubmit={handleSubmit}>
-          <h2>New Recipe</h2>
-          <Field name="name" />
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Field
+                name="name"
+                label="name"
+                variant="outlined"
+                component={TextField}
+              />
+            </Grid>
 
-          <button type="submit">Create</button>
+            <Grid item xs={12}>
+              <Button variant="contained" color="primary" type="submit">
+                Create
+              </Button>
+            </Grid>
+          </Grid>
         </form>
       )}
     </Formik>
