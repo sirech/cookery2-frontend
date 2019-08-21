7b5dbf86904c6800ffc9377cc528a650920c5565
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-08-21 22:47:23 +0200

    basic steps

--- src/new-recipe/NewRecipe.tsx
+++ src/new-recipe/NewRecipe.tsx
@@ -3,20 +3,44 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 
-import { Formik, Field, FormikProps } from 'formik'
+import {
+  Formik,
+  Field,
+  FieldArray,
+  FormikProps,
+  FieldArrayRenderProps
+} from 'formik'
 import { TextField } from 'formik-material-ui'
 
 import * as Yup from 'yup'
 
+interface StepForm {
+  description: string
+  duration: number
+}
 interface RecipeForm {
   name: string
+  steps: StepForm[]
 }
 
 const initialValues: RecipeForm = {
-  name: ''
+  name: '',
+  steps: [{ description: '', duration: 0 }]
 }
 
-const validationSchema = Yup.object().shape({ name: Yup.string().required() })
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required(),
+  steps: Yup.array()
+    .of(
+      Yup.object().shape({
+        description: Yup.string().required(),
+        duration: Yup.number()
+          .min(1)
+          .required()
+      })
+    )
+    .min(1)
+})
 
 const NewRecipe = () => (
   <section data-testid="new-recipe">
@@ -25,7 +49,7 @@ const NewRecipe = () => (
       validationSchema={validationSchema}
       onSubmit={values => console.log(values)}
     >
-      {({ handleSubmit }: FormikProps<RecipeForm>) => (
+      {({ handleSubmit, values }: FormikProps<RecipeForm>) => (
         <form onSubmit={handleSubmit}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
@@ -36,6 +60,46 @@ const NewRecipe = () => (
                 variant="outlined"
                 fullWidth
                 component={TextField}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <FieldArray
+                name="steps"
+                render={({ push, remove }: FieldArrayRenderProps) => (
+                  <>
+                    <h3>Steps</h3>
+                    <Button
+                      onClick={() => push({ description: '', duration: 0 })}
+                    >
+                      Add Step
+                    </Button>
+                    <ul>
+                      {values.steps.map((step, index) => (
+                        <li key={index}>
+                          <Field
+                            id={`steps.${index}.description`}
+                            name={`steps.${index}.description`}
+                            variant="outlined"
+                            fullWidth
+                            component={TextField}
+                          />
+
+                          <Field
+                            id={`steps.${index}.duration`}
+                            name={`steps.${index}.duration`}
+                            type="number"
+                            variant="outlined"
+                            fullWidth
+                            component={TextField}
+                          />
+
+                          <Button onClick={() => remove(index)}>Delete</Button>
+                        </li>
+                      ))}
+                    </ul>
+                  </>
+                )}
               />
             </Grid>
 
