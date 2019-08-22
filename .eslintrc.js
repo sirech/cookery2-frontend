ab95ea8c25cff1160e2350bf3c760062928b6b31
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-08-22 09:16:04 +0200

    add react-hooks linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,19 @@ module.exports = {
     'plugin:jsx-a11y/recommended',
     'plugin:jest/recommended'
   ],
-  plugins: ['@typescript-eslint', 'react', 'prettier', 'jsx-a11y', 'jest'],
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'prettier',
+    'jsx-a11y',
+    'jest',
+    'react-hooks'
+  ],
   rules: {
     '@typescript-eslint/explicit-member-accessibility': 0,
-    '@typescript-eslint/explicit-function-return-type': 0
+    '@typescript-eslint/explicit-function-return-type': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   },
   settings: { react: { version: 'detect' } }
 }
