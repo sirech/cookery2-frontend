5d023510f3995d40d15e4cc53942bacba7e989dc
Mario Fernandez <mario.fernandez@hceris.com>
2021-01-21 23:20:20 +0100

    move provider to upper component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 
-import { Auth0Provider } from '@auth0/auth0-react'
 import { CssBaseline, Container, Box } from '@material-ui/core'
 
 import Navigation from 'navigation'
@@ -10,33 +9,23 @@ import RecipeList from 'recipe-list'
 import RecipeDetails from 'recipe-details'
 import Callback from 'callback'
 
-const host = () => process.env.REACT_APP_HOST || ''
-const redirectUri = () => `${host()}/callback`
-
 const App: React.FC = () => (
   <div data-testid="app">
     <CssBaseline />
 
-    <Auth0Provider
-      domain="hceris.eu.auth0.com"
-      clientId="ThEkgdG1NndLlWoNMcEdEr2KJIs9vKad"
-      scope="openid profile create:recipes"
-      redirectUri={redirectUri()}
-    >
-      <Navigation />
+    <Navigation />
 
-      <Box mt={12}>
-        <Container component="main">
-          <Switch>
-            <Route path="/recipes/new" component={NewRecipe} />
-            <Route path="/recipes/:id" component={RecipeDetails} />
-            <Route path="/recipes" component={RecipeList} />
-            <Route path="/callback" component={Callback} />
-            <Route exact path="/" render={() => <Redirect to="/recipes" />} />
-          </Switch>
-        </Container>
-      </Box>
-    </Auth0Provider>
+    <Box mt={12}>
+      <Container component="main">
+        <Switch>
+          <Route path="/recipes/new" component={NewRecipe} />
+          <Route path="/recipes/:id" component={RecipeDetails} />
+          <Route path="/recipes" component={RecipeList} />
+          <Route path="/callback" component={Callback} />
+          <Route exact path="/" render={() => <Redirect to="/recipes" />} />
+        </Switch>
+      </Container>
+    </Box>
   </div>
 )
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,23 @@ import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
+import { Auth0Provider } from '@auth0/auth0-react'
+
 import App from './App'
+
+const host = () => process.env.REACT_APP_HOST || ''
+const redirectUri = () => `${host()}/callback`
 
 render(
   <BrowserRouter>
-    <App />
+    <Auth0Provider
+      domain="hceris.eu.auth0.com"
+      clientId="ThEkgdG1NndLlWoNMcEdEr2KJIs9vKad"
+      scope="openid profile create:recipes"
+      redirectUri={redirectUri()}
+    >
+      <App />
+    </Auth0Provider>
   </BrowserRouter>,
   document.getElementById('root')
 )
