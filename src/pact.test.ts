c0f002d5cdeca88c22b2f10fea0831df2990d242
Mario Fernandez <mario.fernandez@hceris.com>
2023-01-05 22:09:07 +0100

    patch base url to work on moody os x

--- src/pact.test.ts
+++ src/pact.test.ts
@@ -13,7 +13,7 @@ describe('pacts', () => {
   const recipe = recipeForm()
 
   beforeAll(() => {
-    axios.defaults.baseURL = 'http://localhost:8990'
+    axios.defaults.baseURL = 'http://127.0.0.1:8990'
     jest.restoreAllMocks()
   })
 
