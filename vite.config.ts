a6d5f35ca6d5628884443eee56f5df964e52b07a
Mario Fernandez <mario.fernandez@hceris.com>
2026-01-02 13:43:30 +0100

    fix various problems

--- vite.config.ts
+++ vite.config.ts
@@ -17,5 +17,10 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/setupTests.js'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './coverage',
+    },
   },
 })
